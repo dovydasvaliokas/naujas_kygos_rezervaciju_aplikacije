270e2af84941b567c391f0a5ded94ac567c8e3e5
Vytautas-T <105976888+Vytautas-T@users.noreply.github.com>
2022-10-25 16:07:51 +0300

    roles ir privilegijos

--- src/main/java/lt/knygynas/Knygu/rezervavimas/security/SetupDataLoader.java
+++ src/main/java/lt/knygynas/Knygu/rezervavimas/security/SetupDataLoader.java
@@ -9,16 +9,18 @@ import lt.knygynas.Knygu.rezervavimas.model.repository.VartotojoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 @Component
 public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
-    boolean alreadySetup = false;
+    boolean alreadySetup = true;
 
     @Autowired
     private VartotojoRepository vartotojoRepository;
@@ -29,6 +31,8 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     @Autowired
     private PrivilegijosRepository privilegijosrepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -38,12 +42,17 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
         Privilegijos skaitytiPrivilegije = sukurtiPrivilegijeJeiguJiNerasta("READ_PRIVILEGE");
         Privilegijos rasytiPrivilegije  = sukurtiPrivilegijeJeiguJiNerasta("WHRITE_PRIVILEGE");
 
+        List<Privilegijos> adminPrivilegijos = Arrays.asList(
+                skaitytiPrivilegije, rasytiPrivilegije);
+        sukurtiRoleJeiguJiNerasta("ROLE_ADMIN", adminPrivilegijos);
+        sukurtiRoleJeiguJiNerasta("ROLE_USER", Arrays.asList(skaitytiPrivilegije));
+
         Roles adminRole = rolesRepository.findByPavadinimas("ROLE_ADMIN");
         Vartotojas vartotojas = new Vartotojas();
         vartotojas.setFirstName("vardas");
         vartotojas.setLastName("pavarde");
-        vartotojas.setPassword("slaptazodis");
-        vartotojas.setEmail("ask@as.com");
+        vartotojas.setPassword(passwordEncoder.encode("slaptazodis"));
+        vartotojas.setEmail("true@as.com");
         vartotojas.setRoles(Arrays.asList(adminRole));
         vartotojas.setIjungta(true);
         vartotojoRepository.save(vartotojas);
@@ -52,8 +61,8 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     }
 
     @Transactional
-    Privilegijos sukurtiPrivilegijeJeiguJiNerasta(String vardas) {
-        Privilegijos privilegijos = privilegijosrepository.findByPavadinimas(vardas);
+    Privilegijos sukurtiPrivilegijeJeiguJiNerasta(String pavadinimas) {
+        Privilegijos privilegijos = privilegijosrepository.findByPavadinimas(pavadinimas);
         if (privilegijos == null){
             privilegijos = new Privilegijos();
             privilegijosrepository.save(privilegijos);
@@ -62,7 +71,7 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     }
 
     @Transactional
-    Roles sukurtiRileJeiguJiNerasta(String pavadinimas, Collection<Privilegijos> privilegijos){
+    Roles sukurtiRoleJeiguJiNerasta(String pavadinimas, Collection<Privilegijos> privilegijos){
         Roles role = rolesRepository.findByPavadinimas(pavadinimas);
         if (role == null){
             role = new Roles();
---
+++ src/main/java/lt/knygynas/Knygu/rezervavimas/security/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package lt.knygynas.Knygu.rezervavimas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
